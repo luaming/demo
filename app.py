ac284b74efaa3d0289ecf97c94c13d7214512e7c
luaming <luaming@whu.edu.cn>
2024-07-09 17:21:50 +0800

    modified:   app.py 	deleted:    hello.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-# app.py
-from flask import Flask
-from hello import hello_world
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return hello_world()
+def print_hello_world():
+    return "Hello, World!"
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    message = print_hello_world()
+    return jsonify(message=message)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- hello.py
+++
@@ -1,3 +0,0 @@
-# hello.py
-def hello_world():
-    return "Hello, World!"
